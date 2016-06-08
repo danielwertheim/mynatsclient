9c591fca37969b49fe2ff47079f5c62fe4abd361
Daniel Wertheim <daniel@wertheim.se>
2016-06-08 21:50:05 +0200

    More write optimisations for PUB

--- src/MyNatsClient/Internals/Commands/PubCmd.cs
+++ src/MyNatsClient/Internals/Commands/PubCmd.cs
@@ -4,31 +4,46 @@ namespace MyNatsClient.Internals.Commands
 {
     internal static class PubCmd
     {
+        private static readonly byte[] Cmd = { (byte)'P', (byte)'U', (byte)'B' };
+
         internal static byte[] Generate(string subject, string body, string replyTo = null)
             => Generate(subject, NatsEncoder.GetBytes(body), replyTo);
 
         internal static byte[] Generate(string subject, byte[] body, string replyTo = null)
         {
-            var preBody = GeneratePreBody(subject, body.Length, replyTo);
-            var crlfLen = NatsEncoder.CrlfBytes.Length;
+            var bodyLenString = body.Length.ToString();
+            var preBodyLen = 3 + 1 + subject.Length + (replyTo?.Length ?? 0) + 1 + bodyLenString.Length;
             var buff = new byte[
-                preBody.Length +
-                crlfLen +
+                preBodyLen +
+                NatsEncoder.CrlfBytesLen +
                 body.Length +
-                crlfLen];
-            Array.Copy(preBody, buff, preBody.Length);
-            Array.Copy(NatsEncoder.CrlfBytes, 0, buff, preBody.Length, crlfLen);
-            Array.Copy(body, 0, buff, preBody.Length + crlfLen, body.Length);
-            Array.Copy(NatsEncoder.CrlfBytes, 0, buff, preBody.Length + crlfLen + body.Length, crlfLen);
+                NatsEncoder.CrlfBytesLen];
+
+            FillPreBody(buff, subject, bodyLenString, replyTo);
+            Buffer.BlockCopy(NatsEncoder.CrlfBytes, 0, buff, preBodyLen, NatsEncoder.CrlfBytesLen);
+            Buffer.BlockCopy(body, 0, buff, preBodyLen + NatsEncoder.CrlfBytesLen, body.Length);
+            Buffer.BlockCopy(NatsEncoder.CrlfBytes, 0, buff, preBodyLen + NatsEncoder.CrlfBytesLen + body.Length, NatsEncoder.CrlfBytesLen);
 
             return buff;
         }
 
-        private static byte[] GeneratePreBody(string subject, int bodyLength, string replyTo = null)
+        private static void FillPreBody(byte[] buff, string subject, string bodyLenString, string replyTo = null)
         {
-            var s = replyTo != null ? " " : string.Empty;
+            buff[0] = Cmd[0];
+            buff[1] = Cmd[1];
+            buff[2] = Cmd[2];
+            buff[3] = NatsEncoder.SpaceByte;
 
-            return NatsEncoder.GetBytes($"PUB {subject}{s}{replyTo} {bodyLength}");
+            var curr = 3;
+            curr = NatsEncoder.WriteSingleByteUtf8String(buff, subject, curr);
+
+            if (replyTo != null)
+            {
+                buff[++curr] = NatsEncoder.SpaceByte;
+                curr = NatsEncoder.WriteSingleByteUtf8String(buff, replyTo, curr);
+            }
+            buff[++curr] = (byte)' ';
+            NatsEncoder.WriteSingleByteUtf8String(buff, bodyLenString, curr);
         }
     }
 }
