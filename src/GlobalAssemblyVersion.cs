d50212d62bc7dc269601dc8e4251f6874a03c60a
Daniel Wertheim <daniel@wertheim.se>
2016-06-14 21:23:38 +0200

    Correct version, managed by build script

--- src/GlobalAssemblyVersion.cs
+++ src/GlobalAssemblyVersion.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("0.0.0.*")]
+[assembly: AssemblyInformationalVersion("0.0.0.*")]
