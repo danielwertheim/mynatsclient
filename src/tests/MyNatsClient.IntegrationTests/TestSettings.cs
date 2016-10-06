3c03a1156772a0052ad6b02ea135baa99247bd6e
Daniel Wertheim <daniel@wertheim.se>
2016-10-06 13:43:55 +0200

    Fix so that dotnet test command can be used

--- src/tests/MyNatsClient.IntegrationTests/TestSettings.cs
+++ src/tests/MyNatsClient.IntegrationTests/TestSettings.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Microsoft.Extensions.Configuration;
 using System.Linq;
 
@@ -11,7 +12,7 @@ namespace MyNatsClient.IntegrationTests
         static TestSettings()
         {
             var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory());
+                .SetBasePath(AppContext.BaseDirectory);
 
             builder.AddJsonFile("testsettings.json");
             Config = builder.Build();
