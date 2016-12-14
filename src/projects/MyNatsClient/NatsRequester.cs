2964a2358b88ef12f567d44bb6d1f80fb23ca6b4
Daniel Wertheim <daniel@wertheim.se>
2016-12-14 21:08:12 +0100

    Requester

---
+++ src/projects/MyNatsClient/NatsRequester.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using EnsureThat;
+using MyNatsClient.Ops;
+
+namespace MyNatsClient
+{
+    public class NatsRequester : IDisposable
+    {
+        private readonly string _clientInbox;
+        private readonly INatsClient _client;
+
+        public NatsRequester(INatsClient client)
+        {
+            EnsureArg.IsNotNull(client, nameof(client));
+
+            _client = client;
+            _clientInbox = Guid.NewGuid().ToString("N");
+            _client.Sub($"{_clientInbox}.>", _clientInbox);
+        }
+
+        public async Task<MsgOp> RequestAsync(string subject, string body)
+        {
+            var taskComp = new TaskCompletionSource<MsgOp>();
+            var requestId = Guid.NewGuid().ToString("N");
+            var subscription = _client.MsgOpStream.Subscribe(
+                new DelegatingObserver<MsgOp>(msg =>
+                {
+                    Console.WriteLine(2);
+                    taskComp.SetResult(msg);
+                }),
+                msg => msg.Subject == $"{_clientInbox}.{requestId}");
+
+            await _client.PubAsync(subject, body, $"{_clientInbox}.{requestId}").ConfigureAwait(false);
+
+            return await taskComp.Task.ContinueWith(t =>
+            {
+                subscription?.Dispose();
+                return t.Result;
+            });
+        }
+
+        public void Dispose() => _client.Unsub(_clientInbox);
+    }
+}
