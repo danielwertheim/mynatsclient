872e726a041eb29c4dac1c590ed12059a245fa68
Daniel Wertheim <daniel@wertheim.se>
2016-12-23 12:52:07 +0100

    Create ClientInbox

--- src/projects/MyNatsClient/NatsRequester.cs
+++ src/projects/MyNatsClient/NatsRequester.cs
@@ -8,34 +8,30 @@ namespace MyNatsClient
 {
     public class NatsRequester : IDisposable
     {
-        private readonly string _clientInbox;
         private readonly INatsClient _client;
-        private readonly ISubscription _inboxSubscription;
-        private ObservableOf<MsgOp> _responses;
+        private ClientInbox _inbox;
 
         public NatsRequester(INatsClient client)
         {
             EnsureArg.IsNotNull(client, nameof(client));
 
             _client = client;
-            _clientInbox = Guid.NewGuid().ToString("N");
-            _responses = new ObservableOf<MsgOp>();
-            _inboxSubscription = _client.SubWithHandler($"{_clientInbox}.>", msg => _responses.Dispatch(msg));
+            _inbox = new ClientInbox(client);
         }
 
         public async Task<MsgOp> RequestAsync(string subject, string body)
         {
             var requestId = Guid.NewGuid().ToString("N");
             var taskComp = new TaskCompletionSource<MsgOp>();
-            var requestSubscription = _responses.Subscribe(
+            var requestSubscription = _inbox.Responses.Subscribe(
                 new DelegatingObserver<MsgOp>(msg =>
                 {
                     Console.WriteLine(1);
                     taskComp.SetResult(msg);
                 }),
-                msg => msg.Subject == $"{_clientInbox}.{requestId}");
+                msg => msg.Subject == $"{_inbox.Address}.{requestId}");
 
-            await _client.PubAsync(subject, body, $"{_clientInbox}.{requestId}").ConfigureAwait(false);
+            await _client.PubAsync(subject, body, $"{_inbox.Address}.{requestId}").ConfigureAwait(false);
 
             return await taskComp.Task.ContinueWith(t =>
             {
@@ -49,12 +45,8 @@ namespace MyNatsClient
             Try.All(
                 () =>
                 {
-                    _inboxSubscription?.Dispose();
-                },
-                () =>
-                {
-                    _responses?.Dispose();
-                    _responses = null;
+                    _inbox?.Dispose();
+                    _inbox = null;
                 });
         }
     }
