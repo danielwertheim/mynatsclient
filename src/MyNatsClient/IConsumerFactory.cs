d3e3f066e039bd277927f1ae9da420fa732b8f0b
Daniel Wertheim <daniel@wertheim.se>
2020-09-19 11:27:24 +0200

    Extracts scheduling of consumer (#61)

---
+++ src/MyNatsClient/IConsumerFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyNatsClient
+{
+    /// <summary>
+    /// Responsible for returning a Task that continiously runs the consumer.
+    /// </summary>
+    public interface IConsumerFactory
+    {
+        Task Run(Action consumer, CancellationToken cancellationToken);
+    }
+}
---
+++ src/MyNatsClient/Internals/DefaultTaskSchedulerConsumerFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyNatsClient.Internals
+{
+    internal class DefaultTaskSchedulerConsumerFactory : IConsumerFactory
+    {
+        public Task Run(Action consumer, CancellationToken cancellationToken)
+            => Task.Factory.StartNew(
+                consumer,
+                cancellationToken,
+                TaskCreationOptions.LongRunning,
+                TaskScheduler.Default);
+    }
+}
