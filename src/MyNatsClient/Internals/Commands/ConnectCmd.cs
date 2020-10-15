2effa846cd8fdaba838946237d0c80e401d2339e
Daniel Wertheim <daniel@wertheim.se>
2020-10-15 20:42:43 +0200

    Adds support for specifying connection name

--- src/MyNatsClient/Internals/Commands/ConnectCmd.cs
+++ src/MyNatsClient/Internals/Commands/ConnectCmd.cs
@@ -5,17 +5,22 @@ namespace MyNatsClient.Internals.Commands
 {
     internal static class ConnectCmd
     {
-        internal static ReadOnlySpan<byte> Generate(bool verbose, Credentials credentials)
+        internal static ReadOnlySpan<byte> Generate(bool verbose, Credentials credentials, string name)
         {
-            var opString = GenerateConnectionOpString(verbose, credentials);
+            var opString = GenerateConnectionOpString(
+                verbose,
+                credentials ?? Credentials.Empty,
+                name);
 
             return NatsEncoder.GetBytes(opString).Span;
         }
 
-        private static string GenerateConnectionOpString(bool verbose, Credentials credentials)
+        private static string GenerateConnectionOpString(bool verbose, Credentials credentials, string name)
         {
             var sb = new StringBuilder();
-            sb.Append("CONNECT {\"name\":\"mynatsclient\",\"lang\":\"csharp\",\"protocol\":1,\"pedantic\":false,\"verbose\":");
+            sb.Append("CONNECT {\"name\":\"");
+            sb.Append(name);
+            sb.Append("\",\"lang\":\"csharp\",\"protocol\":1,\"pedantic\":false,\"verbose\":");
             sb.Append(verbose ? "true" : "false");
 
             if (credentials != Credentials.Empty)
