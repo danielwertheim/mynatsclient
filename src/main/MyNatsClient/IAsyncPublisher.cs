a1b8858b244931a11b7f0809089c38738d1b4203
Daniel Wertheim <daniel@wertheim.se>
2021-03-10 22:22:16 +0100

    Splits Publisher by introducing AsyncPublisher

---
+++ src/main/MyNatsClient/IAsyncPublisher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MyNatsClient
+{
+    public interface IAsyncPublisher
+    {
+        Task PubAsync(string subject, string body, string replyTo = null);
+        Task PubAsync(string subject, ReadOnlyMemory<byte> body, string replyTo = null);
+    }
+}
--- src/main/MyNatsClient/Internals/AsyncPublisher.cs
+++ src/main/MyNatsClient/Internals/AsyncPublisher.cs
@@ -1,35 +1,35 @@
-using System;
+﻿using System;
 using System.Threading.Tasks;
 using MyNatsClient.Internals.Commands;
 
 namespace MyNatsClient.Internals
 {
-    internal class Publisher : IPublisher
+    internal class AsyncPublisher : IAsyncPublisher
     {
         private readonly INatsStreamWriter _writer;
         private readonly int _maxPayload;
 
-        internal Publisher(INatsStreamWriter writer, int maxPayload)
+        internal AsyncPublisher(INatsStreamWriter writer, int maxPayload)
         {
             _writer = writer;
             _maxPayload = maxPayload;
         }
 
-        public void Pub(string subject, string body, string replyTo = null)
+        public Task PubAsync(string subject, string body, string replyTo = null)
         {
             var payload = NatsEncoder.GetBytes(body);
             if (payload.Length > _maxPayload)
                 throw NatsException.ExceededMaxPayload(_maxPayload, payload.Length);
 
-            PubCmd.Write(_writer, subject, replyTo, payload);
+            return PubCmd.WriteAsync(_writer, subject.AsMemory(), replyTo.AsMemory(), payload);
         }
 
-        public void Pub(string subject, ReadOnlyMemory<byte> body, string replyTo = null)
+        public Task PubAsync(string subject, ReadOnlyMemory<byte> body, string replyTo = null)
         {
             if (body.Length > _maxPayload)
                 throw NatsException.ExceededMaxPayload(_maxPayload, body.Length);
 
-            PubCmd.Write(_writer, subject, replyTo, body);
+            return PubCmd.WriteAsync(_writer, subject.AsMemory(), replyTo.AsMemory(), body);
         }
     }
 }
