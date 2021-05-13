9bc11d9bfeec52644b327b40f81c598987f6141c
Daniel Wertheim <daniel@wertheim.se>
2021-05-13 21:56:35 +0200

    Using field for internal state

--- src/main/MyNatsClient/LoggerManager.cs
+++ src/main/MyNatsClient/LoggerManager.cs
@@ -6,16 +6,16 @@ namespace MyNatsClient
 {
     public static class LoggerManager
     {
-        private static ILoggerFactory Factory { get; set; } = NullLoggerFactory.Instance;
+        private static ILoggerFactory _factory = NullLoggerFactory.Instance;
 
         public static ILogger CreateLogger(Type type)
-            => Factory.CreateLogger(type);
+            => _factory.CreateLogger(type);
 
         public static ILogger<T> CreateLogger<T>()
-            => Factory.CreateLogger<T>();
+            => _factory.CreateLogger<T>();
 
         public static void UseFactory(ILoggerFactory factory)
-            => Factory = factory;
+            => _factory = factory;
 
         public static void ResetToDefaults()
             => UseFactory(NullLoggerFactory.Instance);
